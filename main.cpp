1a59cfe6954f800d3df7067c72dff4b690759057
Tri Pham <trimpham98@gmail.com>
2022-10-28 21:55:37 -0700

    Add TODO comments and initialize number of coin variables

--- main.cpp
+++ main.cpp
@@ -5,15 +5,17 @@ int main()
 {
     double userAmount;
 
+    int numPennies = 0, numNickels = 0, numDimes = 0, numQuarters = 0;
+
     cout << "This program calculates how many coins are needed for change." << endl;
 
-    // Prompt user for amount 
+    // Prompt user for amount
     cout << "Please enter the amount you want change for: $";
     cin >> userAmount;
 
-    // TODO: Use conditionals and modulus to see how many coins are needed
+    // TODO: Use if/else if/else conditionals and modulus to see how many coins are needed
 
-    // TODO: 
+    // TODO: Print out how many pennies, nickels, dimes, and quarters are needed
 
     return 0;
 }
