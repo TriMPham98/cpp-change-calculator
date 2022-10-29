11abc2e2428cd1fc63423d8ee0fd4fb5532a7514
Tri Pham <trimpham98@gmail.com>
2022-10-28 21:48:36 -0700

    Prompt the user for an amount to calculate change for

--- main.cpp
+++ main.cpp
@@ -3,7 +3,17 @@ using namespace std;
 
 int main()
 {
+    double userAmount;
 
+    cout << "This program calculates how many coins are needed for change." << endl;
+
+    // Prompt user for amount 
+    cout << "Please enter the amount you want change for: $";
+    cin >> userAmount;
+
+    // TODO: Use conditionals and modulus to see how many coins are needed
+
+    // TODO: 
 
     return 0;
 }
