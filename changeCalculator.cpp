475c9968021e7e03838a77ce0549f62467c60a5a
Tri <trimpham98@gmail.com>
2022-12-08 23:18:46 -0800

    Rewrite repeated if statements with a loop

--- changeCalculator.cpp
+++ changeCalculator.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     bool changeCalcLoop = true;
-    double userAmount = 0;
+    double totalAmount = 0;
     int remainingAmount = 0;
     int numOneDollars = 0, numFiveDollars = 0, numTenDollars = 0, numTwentyDollars = 0, numFiftyDollars = 0, numHundredDollars = 0;
     int numPennies = 0, numNickels = 0, numDimes = 0, numQuarters = 0;
@@ -16,108 +16,34 @@ int main()
     {
         // Prompt user for amount
         cout << "\nPlease enter an amount you want change for or enter 0 to exit: $";
-        cin >> userAmount;
+        cin >> totalAmount;
 
-        if (userAmount == 0)
+        if (totalAmount == 0)
         {
             changeCalcLoop = false;
             break;
         }
 
         // Use modulus based on coin value to see how many coins are needed
-        remainingAmount = static_cast<int>(userAmount * 100);
+        remainingAmount = static_cast<int>(totalAmount * 100);
 
-        // Number of $100's
-        numHundredDollars = remainingAmount / 10000;
-        remainingAmount %= 10000;
+        // Loop over denominations of bills and coins
+        int denominations[] = {10000, 5000, 2000, 1000, 500, 100, 25, 10, 5, 1};
+        string names[] = {"$100 Dollar Bills", "$50 Dollar Bills", "$20 Dollar Bills", "$10 Dollar Bills", "$5 Dollar Bills", "$1 Dollar Bills",
+                          "25¢ Quarters", "10¢ Dimes", "5¢ Nickels", "1¢ Pennies"};
 
-        // Number of $50's
-        numFiftyDollars = remainingAmount / 5000;
-        remainingAmount %= 5000;
-
-        // Number of $20's
-        numTwentyDollars = remainingAmount / 2000;
-        remainingAmount %= 2000;
-
-        // Number of $10's
-        numTenDollars = remainingAmount / 1000;
-        remainingAmount %= 1000;
-
-        // Number of $5's
-        numFiveDollars = remainingAmount / 500;
-        remainingAmount %= 500;
-
-        // Number of $1's
-        numOneDollars = remainingAmount / 100;
-        remainingAmount %= 100;
-
-        // Number of 25¢'s
-        numQuarters = remainingAmount / 25;
-        remainingAmount %= 25;
-
-        // Number of 10¢'s
-        numDimes = remainingAmount / 10;
-        remainingAmount %= 10;
-
-        // Number of 5¢'s
-        numNickels = remainingAmount / 5;
-        remainingAmount %= 5;
-
-        // Number of 1¢'s
-        numPennies = remainingAmount;
-
-        // Print out how many dollars and coins are required for the change
-        if (userAmount > 0)
+        for (int i = 0; i < 10; i++)
         {
-            cout << "The change required for $" << fixed << setprecision(2) << userAmount << " is: " << endl;
+            int num = remainingAmount / denominations[i];
+            remainingAmount %= denominations[i];
+
+            if (num > 0)
+            {
+                cout << num << "\t" << names[i] << endl;
+            }
         }
 
-        if (numHundredDollars > 0)
-        {
-            cout << numHundredDollars << "\t$100\tDollar Bills" << endl;
-        }
-        if (numFiftyDollars > 0)
-        {
-            cout << numFiftyDollars << "\t$50\tDollar Bills" << endl;
-        }
-        if (numTwentyDollars > 0)
-        {
-            cout << numTwentyDollars << "\t$20\tDollar Bills" << endl;
-        }
-        if (numTenDollars > 0)
-        {
-            cout << numTenDollars << "\t$10\tDollar Bills" << endl;
-        }
-        if (numFiveDollars > 0)
-        {
-            cout << numFiveDollars << "\t$5\tDollar Bills" << endl;
-        }
-        if (numOneDollars > 0)
-        {
-            cout << numOneDollars << "\t$1\tDollar Bills" << endl;
-        }
         cout << endl;
-        if (numQuarters > 0)
-        {
-            cout << numQuarters << "\t25¢\tQuarters" << endl;
-        }
-        if (numDimes > 0)
-        {
-            cout << numDimes << "\t10¢\tDimes" << endl;
-        }
-        if (numNickels > 0)
-        {
-            cout << numNickels << "\t5¢\tNickels" << endl;
-        }
-        if (numPennies > 0)
-        {
-            cout << numPennies << "\t1¢\tPennies" << endl;
-        }
-
-        if (userAmount < 0)
-        {
-            cout << "Please enter a positive amount." << endl;
-        }
     }
 
     return 0;
